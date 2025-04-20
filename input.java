fb1bf4388d906c391010610110378cc6b20abeb6
chetan221204 <chetanchauhan247@gmail.com>
2025-04-20 22:50:37 +0530

    *

---
+++ input.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class input {
+
+    public static void main(String[] args) {
+         Scanner in =new Scanner(System.in);
+        // System.out.println("Enter age");
+        // int age = in.nextInt();
+        // if (age>=18) {
+        //     System.out.println("YOu are Adult");
+            
+        // }
+        // else
+        // System.out.println("You are Child");
+        // String name = in .next(); //for single  world
+        // System.out.println(name);
+
+        String name = in.nextLine(); //for a line
+        System.out.println(name);
+    }
+}
--- mathclass.java
+++ mathclass.java
@@ -1,8 +1,13 @@
 public class mathclass {
     public static void main(String[] args) {
-        int a=54;
-        int b=52;
-        System.out.println(Math.max(a, b));
+    //     int a=54;
+    //     int b=52;
+    //     System.out.println("Max no is:");
+    //     System.out.println(Math.max(a, b)); //Max
+    //     System.out.println("Min no. is :");
+    //     System.out.println( Math.min(a, b)); //Min
+
+         System.out.println((int)(Math.random()*100));
     }
     
 }

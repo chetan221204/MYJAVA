8cc2a4f54d3fa266aac9388dc863899bf14c0131
Chetan Chauhan <chetanchauhan247@gmail.com>
2026-02-26 13:08:20 +0530

    today

---
+++ hashmap.java
@@ -0,0 +1,79 @@
+package MYJAVA;
+
+public class hashmap {
+    // public class hello {
+//     public static void main(String[] args) {
+//         interface A{
+//             void run();
+//          }
+//          interface B{
+//             void run();
+//          }
+//          class C implements A,B{
+//             public void run(){
+//                 System.out.println("Hello");
+//             }
+
+//          }
+//     }
+// 
+
+// import java.util.ArrayList;
+
+// public class hello {
+
+//     public static void main(String[] args) {
+//         // int arr[]=new int[5];
+
+//         ArrayList<Integer> list=new ArrayList<>();
+//         list.add(4);
+//         list.add(5);
+//         list.add(6);
+//         list.add(7);
+//         list.addFirst(1);
+//         for(int i=0;i<list.size();i++){
+//             System.out.println(list.get(i));
+//         }
+
+//         System.out.println(list);
+//         System.out.println(list.size());
+//         System.out.println(list.get(1));
+//         System.out.println(list.set(0, 0));
+//         System.out.println(list);
+//         System.out.println(list.contains(5));
+        
+         
+//     }
+// }
+
+// import java.util.HashMap;
+
+// public class hello {
+
+//     public static void main(String[] args) {
+//         HashMap<Integer,Integer> map =new HashMap<>();
+//         map.put(0, 10);
+//         map.put(1, 10);
+//         map.put(2, 10);
+
+//         System.out.println(map);
+//         System.out.println(map.get(0));
+//         map.remove(0);
+//         System.out.println(map.containsKey(2));
+//         System.out.println(map);
+//     }
+// }
+
+import java.util.HashMap;
+
+public class hello {
+
+    public static void main(String[] args) {
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<str.lenght();i++){
+            Char ch =str.CharAt(i);
+            map.put(ch.getOrDefault(ch. default))
+        }
+    }
+}    
+}
